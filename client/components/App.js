9f8dd0798ca19c070c0d98fd114d717fbbebee7d
Damien Outar <damienoutar@gmail.com>
2021-03-25 21:55:26 -0400

    started to add redux to app

--- client/components/App.js
+++ client/components/App.js
@@ -2,8 +2,14 @@ import React from "react"
 //import any sub-components
 import Campuses from './Campuses'
 import Students from './Students'
+import Home from './Home'
 import axios from 'axios'
 import {HashRouter,Route,Link} from 'react-router-dom'
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
+
+function reducer () {}
+const store=createStore(reducer)
 
 export default class App extends React.Component {
 	//constructor to initialize state
@@ -18,22 +24,39 @@ export default class App extends React.Component {
 	async componentDidMount(){
         let response=await axios.get(`/campus`)
         const allCampus=response.data
-		this.setState({campuses:allCampus})
 		response=await axios.get(`/student`)
         const allStudent=response.data
-        this.setState({students:allStudent})
+        this.setState({students:allStudent,campuses:allCampus})
     }
 	//any custom methods
 	//render
 	render(){
 		return(
-			<HashRouter>
-				<div>
-					Welcome to Educational Database
-					<Route path='/campuses'> <Campuses campuses={this.state.campuses}/> </Route> {/*react-router v5 syntax*/}
-					<Route path='/students'> <Students students={this.state.students}/> </Route> {/*react-router v5 syntax*/}
-				</div>
-			</HashRouter>
+			<Provider store={store}>
+				<HashRouter>
+					<Route exact path='/'>
+						<div className='navbar'>
+							<div><Link to='/campuses'>Campuses</Link></div>
+							<div><Link to='/students'>Students</Link></div>
+						</div>
+						<Home/>
+					</Route>
+					<Route path='/campuses'>
+						<div className='navbar'>
+							<div><Link to='/'>Home</Link></div>
+							<div><Link to='/students'>Students</Link></div>
+						</div> 
+						<Campuses campuses={this.state.campuses}/> 
+					</Route> {/*react-router v5 syntax*/}
+					<Route path='/students'> 
+						<div className='navbar'>
+							<div><Link to='/'>Home</Link></div>
+							<div><Link to='/campuses'>Campuses</Link></div>
+						</div> 
+						<Students students={this.state.students}/> 
+					</Route> {/*react-router v5 syntax*/}
+				</HashRouter>
+			</Provider>
 		)
 	}
 }
---
+++ client/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home=(props)=>{
+    return(
+        <div>
+            Welcome to Educational Database
+        </div>
+    )
+
+}
+
+export default Home
